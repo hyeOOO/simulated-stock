84a83618d4192929462432c1b5919466538a3314
hyeOOO <hyewon5342@nate.com>
2024-05-25 14:39:08 +0900

    first commit

---
+++ src/main/java/toyproject/simulated_stock/SimulatedStockApplication.java
@@ -0,0 +1,13 @@
+package toyproject.simulated_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatedStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulatedStockApplication.class, args);
+	}
+
+}
